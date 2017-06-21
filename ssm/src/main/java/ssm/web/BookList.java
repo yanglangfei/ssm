396f3edfae495cad888f96bd9fd21e572098e0c6
langfei yang <185601452@qq.com>
2017-06-21 18:44:28 +0800

    ssm list

--- ssm/src/main/java/ssm/web/BookList.java
+++ ssm/src/main/java/ssm/web/BookList.java
@@ -1,15 +1,12 @@
 package ssm.web;
 import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import ssm.dto.AppointExecution;
 import ssm.dto.Result;
 import ssm.entity.Book;
@@ -24,6 +21,7 @@ public class BookList {
 
 	@Autowired
 	BookService bookService;
+
 	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
 	public String queryBooks(@PathVariable("bookId") Long bookId, Model model) {
 		if (bookId == null) {
@@ -44,20 +42,30 @@ public class BookList {
 		return "list";
 	}
 	
-	@RequestMapping(value="/{bookId}/appoint",method=RequestMethod.POST,produces="application/json; charset=utf-8")
-	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId,@PathVariable("studentId") Long studentId){
-		if(studentId==null||studentId.equals("")){
-			return new Result<>(false,"学号不能为空");
+	
+
+	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = { "application/json; charset=utf-8" })
+	private Result<AppointExecution> appoint(
+			@PathVariable("bookId") Long bookId,
+			@PathVariable("studentId") Long studentId) {
+		if (studentId == null || studentId.equals("")) {
+			System.out.println("学号不能为空");
+			return new Result<>(false, "学号不能为空");
 		}
-		AppointExecution execution=null;
+		AppointExecution execution = null;
 		try {
-			execution=bookService.appoint(bookId, studentId);
+			execution = bookService.appoint(bookId, studentId);
 		} catch (NoNumberException e) {
-			execution=new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
-		}catch (RepeatAppointException e) {
-			execution=new AppointExecution(bookId,AppointStateEnum.REPEAT_APPOINT);
-		}catch (Exception e) {
-			execution=new AppointExecution(bookId,AppointStateEnum.INNER_ERROR);
+			System.out.println("NoNumberException");
+			execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
+		} catch (RepeatAppointException e) {
+			System.out.println("RepeatAppointException");
+			execution = new AppointExecution(bookId,
+					AppointStateEnum.REPEAT_APPOINT);
+		} catch (Exception e) {
+			execution = new AppointExecution(bookId,
+					AppointStateEnum.INNER_ERROR);
+			System.out.println("Exception");
 		}
 		return new Result<>(true, execution);
 	}
